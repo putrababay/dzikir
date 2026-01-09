b7a4a245d3f00a688afee3abce2d919e8a824306
Bayu Anggara Putra <58971800+putrababay@users.noreply.github.com>
2026-01-09 09:46:10 +0700

    aaaa

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
-// Simpan dengan nama sw.js di folder yang sama dengan index.html
 const CACHE_NAME = 'tasbih-mu-v1';
-const assets = [
+const ASSETS = [
     './',
     './index.html',
-    './manifest.json',
-    'https://cdn.jsdelivr.net/npm/sweetalert2@11',
-    'https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3'
+    './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Install: Simpan aset dasar saja agar tidak berat
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Aktivasi: Hapus cache lama jika versi berubah
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch: Strategi Network First (Coba internet dulu, kalau gagal baru cache)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
